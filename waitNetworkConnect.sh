580becc6a73e098eff21738c264d54724ee72702
coco-server <cocobird231@gmail.com>
2023-07-11 15:13:54 +0800

    Add requirement and source files

--- waitNetworkConnect.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-reg='[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'
-printf "%s" "Network connecting..."
-while ! ip addr show dev $1 | grep -P $reg &> /dev/null
-do
-    printf "%c" "."
-done
-printf "\n%s\n" "Network detected."
-# Process here
